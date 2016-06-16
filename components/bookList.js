e1ce1f932c7b7b59d3e4b7864526527575e348ff
Robin Venneman <robin.venneman@marlon.be>
2016-06-16 20:20:38 +0200

    Add ul

--- components/bookList.js
+++ components/bookList.js
@@ -4,7 +4,9 @@ import Book from './book';
 const BookList = (props) => {
 	return (
 		<section className={ props.className }>
-			{ props.books.map( book => <Book key={book.title} book={book} />) }
+			<ul>
+				{ props.books.map( book => <Book key={book.title} book={book} />) }
+			</ul>
 		</section>
 	);
 };
